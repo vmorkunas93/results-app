cf29b9179bf4c6641ed03f2a749a96b939244100
Vytautas <vytautas.morkunas93@gmail.com>
2019-08-10 13:02:24 +0300

    Modified Percentage in Stats

--- client/src/components/Stats.jsx
+++ client/src/components/Stats.jsx
@@ -1,31 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Container } from 'reactstrap';
-import Spinner from './LoadingSpinner'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container } from "reactstrap";
+import Spinner from "./LoadingSpinner";
 
 const Stats = () => {
   const [stats, setStats] = useState([]);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchStats = async () => {
-      setLoading(true)
+      setLoading(true);
       const statsRes = await axios(`/getStats`);
       setStats(statsRes.data);
-      setLoading(false)
+      setLoading(false);
     };
     fetchStats();
   }, []);
 
   return (
     <Container>
-      {loading ? <Spinner /> : (
+      {loading ? (
+        <Spinner />
+      ) : (
         <div>
           <h5>Statistika</h5>
-          <p>Iš viso sužaista rungtynių: {stats['gamesCount']}</p>
-          <p>Darius laimėjo: {stats['DariusPlayed'] ? (stats['DariusWon'] + '/' + stats['DariusPlayed'] + ' - ' + Math.floor((stats['DariusWon'] / stats['DariusPlayed']) * 100) + '%') : '0/0 - 0%'}</p>
-          <p>Erikas laimėjo: {stats['ErikasWon']}/{stats['ErikasPlayed']} - {Math.floor((stats['ErikasWon'] / stats['ErikasPlayed']) * 100)}%</p>
-          <p>Vytautas laimėjo: {stats['VytautasWon']}/{stats['VytautasPlayed']} - {Math.floor((stats['VytautasWon'] / stats['VytautasPlayed']) * 100)}%</p>
+          <p>Iš viso sužaista rungtynių: {stats["gamesCount"]}</p>
+          <p>
+            Darius laimėjo:{" "}
+            {stats["DariusPlayed"]
+              ? stats["DariusWon"] +
+                "/" +
+                stats["DariusPlayed"] +
+                " - " +
+                ((stats["DariusWon"] / stats["DariusPlayed"]) * 100).toFixed(
+                  2
+                ) +
+                "%"
+              : "0/0 - 0.00%"}
+          </p>
+          <p>
+            Erikas laimėjo: {stats["ErikasWon"]}/{stats["ErikasPlayed"]} -{" "}
+            {((stats["ErikasWon"] / stats["ErikasPlayed"]) * 100).toFixed(2)}%
+          </p>
+          <p>
+            Vytautas laimėjo: {stats["VytautasWon"]}/{stats["VytautasPlayed"]} -{" "}
+            {((stats["VytautasWon"] / stats["VytautasPlayed"]) * 100).toFixed(
+              2
+            )}
+            %
+          </p>
         </div>
       )}
     </Container>
